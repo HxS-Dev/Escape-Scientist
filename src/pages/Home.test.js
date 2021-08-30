84c50539877560b26e1a452960088e728cc52c9a
Hamad <kashmirh@coventry.ac.uk>
2021-08-30 15:43:44 +0100

    testing configured with tiny example

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
+//for database things (if we have, use beforeEach and afterEach with a test db)
 
-describe('First test Example', () => {
-    it('1 should not be 3', () => {
-        expect(1).toBe(3);
+describe('Test Layout Example', () => {
+    test('1 should not be 3', () => {
+        expect(1).not.toBe(3);
+    });
+});
+
+// shallow is for single component
+// mount when you need to test children 
+describe('Enzyme example', () => {
+    test('H1 tag is rendered correctly', () => {
+        const wrapper = shallow(<Home />);
+        // shows whole component jsx
+        console.log(wrapper.debug())
+        expect(wrapper.find('h1').first().text()).toBe("Home Page");
     });
 });
