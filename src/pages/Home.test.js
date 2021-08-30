9a47d3dae680cf63d82f93280ed796b67e20ae48
Hamad <kashmirh@coventry.ac.uk>
2021-08-30 15:10:02 +0100

    remove unnecessary

---
+++ src/pages/Home.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from './Home';
+
+describe('First test Example', () => {
+    it('1 should not be 3', () => {
+        expect(1).toBe(3);
+    });
+});
