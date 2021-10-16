09efddff824487109e8d2267517c822274a00f9e
Hamad <kashmirh@coventry.ac.uk>
2021-10-16 15:35:53 +0100

    more file setup, transitioned to stylesheets, started navbar structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+// * styles
+import "./App.scss";
+// * Components
 import React from "react";
-import Nav from './components/Nav';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Playground from './pages/Playground';
-import Portfolio from './pages/Portfolio';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Playground from "./pages/Playground";
+import Portfolio from "./pages/Portfolio";
+// * dependencies
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
- 
 function App() {
   return (
     <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/portfolio" component={Portfolio} />
-          {/* protected routes will come in at some point */}
-          <Route path="/playground" component={Playground} />
-        </Switch>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
+        {/* protected routes will come in at some point */}
+        <Route path="/playground" component={Playground} />
+      </Switch>
     </Router>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,31 @@
+// * Dependencies
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+// * Components
+import TransparentContainer from "./TransparentContainer";
+//* Styles
+import "./styles/Nav.scss";
 function Nav() {
   return (
-    <ul>
-        <Link to='/'>
-            <li>Home</li>   
+    <TransparentContainer additionalClasses="navbar">
+      <ul>
+        <Link to="/">
+          <li>Home</li>
         </Link>
-        <Link to='/about'>
-            <li>About</li>   
+        <Link to="/about">
+          <li>About</li>
         </Link>
-        <Link to='/contact'>
-            <li>Contact</li>   
+        <Link to="/contact">
+          <li>Contact</li>
         </Link>
-        <Link to='/playground'>
-            <li>Playground</li>   
+        <Link to="/playground">
+          <li>Playground</li>
         </Link>
-        <Link to='/portfolio'>
-            <li>Portfolio</li>   
+        <Link to="/portfolio">
+          <li>Portfolio</li>
         </Link>
-    </ul>
+      </ul>
+    </TransparentContainer>
   );
 }
 
