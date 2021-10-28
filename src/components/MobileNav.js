136d7c4897cc249dca2393fa2eb5444646824809
Hamad <kashmirh@coventry.ac.uk>
2021-10-28 21:21:04 +0100

    hide and show navbar at breakpoints

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -5,7 +5,7 @@ import React from "react";
 import "./styles/MobileNav.scss";
 
 const MobileNav = () => {
-  return <div></div>;
+  return <h1 className="mobile-navbar">Hello</h1>;
 };
 
 export default MobileNav;
