94ac0b58193acc77ff319188cce7cfc347d4eb1d
Hamad <hamad.kashmiri@space01.co.uk>
2021-12-11 14:43:35 +0000

    changes

--- src/pages/MainDisplay.jsx
+++ src/pages/MainDisplay.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 
-export const MainDisplay = () => {
+export const MainDisplay = ({ showClue }) => {
+  // lifting the props move state to control display
+  // provide app with scanner state using context api or alternative
   const [isClueVisible, setIsClueVisible] = useState(false);
 
   const handleShowClue = (event) => {
     console.log(event);
   };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="timer card"></div>
-        </div>
-      </div>
+    <div className="container-fluid">
+      <div className="timer"></div>
       <div className="row">
         <div className="col">
           <div className="clues" onClick={handleShowClue}></div>
