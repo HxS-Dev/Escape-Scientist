c3fb31efcfb791ae771e6c34792bce815088d55a
Hamad <hamad.kashmiri@space01.co.uk>
2021-12-11 14:02:23 +0000

    App reset

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 // * styles
-import "./App.scss";
 // * Components
 import React from "react";
-import Nav from "./components/Nav";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Playground from "./pages/Playground";
-import Portfolio from "./pages/Portfolio";
-import MobileNav from "./components/MobileNav";
+
 // * dependencies
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return <div></div>;
